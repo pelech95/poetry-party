93b332a8fffe2dfa5f6a9c4e5a462b19ee91bb97
elena <elenapelech95@gmail.com>
2018-02-02 11:03:53 -0600

    adding in comments to shell script for verb

--- wordcount.sh
+++ wordcount.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# get rid of comment lines
+# search for word (case insensitive, whole words only)
+# translate spaces to newlines
+#search for word (case insensitive, whole words only)
+# word count by line to get total number of occurances)
+# word count 
 grep -v "^#" $2 | grep -i -w $1 | tr ' ' '\n' | grep -i -w $1 | wc -l
 
